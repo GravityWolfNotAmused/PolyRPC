b202f9c4b1da6ce2f24052f26f37224266adda78
Larry <gravitywolfnotamused@gmail.com>
2020-04-17 19:47:16 -0600

    Update

--- PolyRPC/Addons/PolyRPC/Scripts/4_World/Plugins/PluginManager.c
+++ PolyRPC/Addons/PolyRPC/Scripts/4_World/Plugins/PluginManager.c
@@ -11,11 +11,43 @@ modded class PluginManager
 		GetDayZGame().Event_OnRPC.Insert( OnRPC );
 	}
 
+	override void Init()
+	{
+		super.Init();
+
+		RegisterPlugin("PolyUIManager", true, false);
+	}
+
 	void OnRPC(PlayerIdentity sender, Object target, int rpc_type, ParamsReadContext ctx)
 	{
 		foreach(typename type, PluginBase plugin : m_PluginsPtrs)
 		{
 			plugin.OnRPC(sender, rpc_type, ctx);
+		}
+	}
+}
+
+class PolyUIManager : PluginBase
+{
+	override void OnRPC(PlayerIdentity sender, int rpc_type, ParamsReadContext ctx)
+	{
+		if(GetGame().IsClient())
+		{
+			UIManager manager = GetGame().GetUIManager();
+			UIScriptedMenu menu = manager.GetMenu();
+			PolyUIScriptedMenu polyMenu = PolyUIScriptedMenu.Cast(menu);
+
+			while (menu)
+			{
+				menu = menu.GetParentMenu();
+				polyMenu = PolyUIScriptedMenu.Cast(menu);
+				
+				if(polyMenu)
+				{
+					menu.OnRPC(sender, rpc_type, ctx);
+					break;
+				}
+			}
 		}
 	}
 }
---
+++ PolyRPC/Addons/PolyRPC/Scripts/4_World/Tools/PolyUIScriptedMenu.c
@@ -0,0 +1,57 @@
+class PolyUIScriptedMenu extends UIScriptedMenu
+{
+	protected ref map<int, ref IRPCExecutable> rpcs;
+
+	void PolyUIScriptedMenu()
+	{
+		rpcs = new map<int, ref IRPCExecutable>;
+		
+		InitRPCs();
+	}
+
+	void ~PolyUIScriptedMenu()
+	{
+		delete rpcs;
+	}
+
+	/*
+	*	An Function to initalize RPCs. To Be Overriden by user.
+	*/
+	void InitRPCs()
+	{
+	}
+
+	/*
+	*	Function to add RPCs to Hashmap
+	*	@param type Typename of the RPC class
+	*	@since 0.1
+	*/
+	void AddRPC(typename type, int id)
+	{
+		ref IRPCExecutable rpc = type.Spawn();
+
+		if(rpc != null)
+		{
+			rpcs.Insert(id, rpc);
+		}
+	}
+
+	/**
+	 * Checks to see if RPC is stored inside of class and executes it.
+	 *
+	 * @param sender Player Identity of the player who sent it.
+	 * @param rpc_type Integer type which the rpc is identitfied.
+	 * @param ctx   Class which caches data which is stored before being sent over network.
+	 * @since             0.1
+	 */
+	void OnRPC(PlayerIdentity sender, int rpc_type, ParamsReadContext ctx)
+	{
+		autoptr IRPCExecutable rpc = rpcs.Get(rpc_type);
+
+		if(rpc != null)
+		{
+			rpc.ExecuteRPC(sender, null, ctx, this);
+			return;
+		}
+	}
+}
